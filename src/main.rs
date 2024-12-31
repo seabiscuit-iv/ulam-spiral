22daf51038866434653784a51c246919bb089127
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2024-12-30 20:01:07 -0800

    implemented scaling

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use macroquad::{prelude::*, time};
 const FONT_SIZE : f32 = 40.0;
 const X_SPACE : f32 = 40.0;
 const Y_SPACE : f32 = 40.0;
+const RADIUS: f32 = 5.0;
 
 const NUMBERS: bool = false;
-const RADIUS: f32 = 5.0;
 
 enum Side {
     Right,
@@ -20,15 +20,20 @@ enum Side {
 #[macroquad::main("Main")]
 async fn main() {    
     use Side::*;
+
+    let mut scale = 1.0;
+
     loop {
         clear_background(BLACK);
 
         let (w, h) = (screen_width(), screen_height());
 
+        let limit = (2*(scale as u32 + 8)).pow(3);
+
         // draw_circle(w/2.0, h/2.0, 8.0, WHITE);
-        for i in 1..400_u32 {
+        for i in 1..limit {
             if i == 1 {
-                draw_num(i, (0, 0), (w, h));
+                draw_num(i, (0, 0), (w, h), scale);
 
                 continue;
             } 
@@ -83,22 +88,24 @@ async fn main() {
 
             
             //text drawing
-            draw_num(i, coords, (w, h));
+            draw_num(i, coords, (w, h), scale);
         }
+
+        scale += time::get_frame_time() / 4.0;
 
         next_frame().await
     }
 }
 
 
-fn draw_num(num: u32, coords: (i32, i32), center: (f32, f32)) {
+fn draw_num(num: u32, coords: (i32, i32), center: (f32, f32), scale: f32) {
     
     let fontsize = FONT_SIZE / ((1.18_f32).powf((num as f32).log10().floor() + 1.0));
 
     let text_dimensions = measure_text(&num.to_string(), None, fontsize as u16, 1.0);
 
-    let x = center.0/2.0 +  X_SPACE * coords.0 as f32;
-    let y = center.1/2.0 - Y_SPACE * coords.1 as f32;
+    let x = (center.0/2.0 +  X_SPACE * coords.0 as f32 * (1.0/scale));
+    let y = (center.1/2.0 - Y_SPACE * coords.1 as f32 * (1.0/scale));
 
     //text size debugging
     // draw_rectangle_lines(x - text_dimensions.width/2.0, y - text_dimensions.height/2.0, text_dimensions.width, text_dimensions.height, 1.0, RED);
@@ -106,7 +113,7 @@ fn draw_num(num: u32, coords: (i32, i32), center: (f32, f32)) {
     if NUMBERS {
         draw_text(&num.to_string(), x - text_dimensions.width/2.0, y + text_dimensions.height/2.0, fontsize, WHITE);
     } else {
-        draw_circle(x, y, RADIUS, WHITE);
+        draw_circle(x, y, RADIUS / scale, WHITE);
     }
     
 }
